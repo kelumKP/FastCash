8e33300f8703b735e7d150d60e3e71f20537b08f
kelumkp <kelumgit@outlook.com>
2024-08-24 23:39:38 +1200

    Merge loanApp-client contents into the main repository

---
+++ Frontend/loanApp-client/dist/out-tsc/app/app.routes.js
@@ -0,0 +1,6 @@
+import { LoanApplicationComponent } from './features/customer/loan-application/loan-application.component';
+export const routes = [
+    { path: '', redirectTo: 'loan-application', pathMatch: 'full' },
+    { path: 'loan-application', component: LoanApplicationComponent }
+];
+//# sourceMappingURL=app.routes.js.map
---
+++ Frontend/loanApp-client/dist/out-tsc/app/features/customer/customer-routing.module.js
@@ -0,0 +1,14 @@
+import { __decorate } from "tslib";
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+const routes = [];
+let CustomerRoutingModule = class CustomerRoutingModule {
+};
+CustomerRoutingModule = __decorate([
+    NgModule({
+        imports: [RouterModule.forChild(routes)],
+        exports: [RouterModule]
+    })
+], CustomerRoutingModule);
+export { CustomerRoutingModule };
+//# sourceMappingURL=customer-routing.module.js.map
